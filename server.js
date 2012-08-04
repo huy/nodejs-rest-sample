d5b807bc5f8618c043d5465787ecca9b5277e1aa
Le Huy <lehuy20@gmail.com>
2012-08-04 10:18:46 +0700

    log result of insert

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.bodyParser());
 
 // convenient debug function
 function log(message, obj) {
-  console.log(message +  ": " + JSON.stringify(obj, null, '\t'));
+  if( typeof obj !== "undefined" )	
+    console.log(message +  ": " + JSON.stringify(obj, null, '\t'));
+  else
+    console.log(message);
 };
 
 var db = require('db');
@@ -25,7 +28,7 @@ app.get('/notification/:id', function(req, res){
     conn.collection('doc', function(err, collection) {
       var result = collection.find().toArray(function(err, items) {});
 
-      log("call find on collection doc",result);
+      log("call find on collection doc", result);
 
       if( typeof result !== "undefined" && result.length > 0 ) 
         res.json(result.shift());
@@ -47,7 +50,9 @@ app.post('/notification', function(req, res) {
 
   db.connect(function(conn){
     conn.collection('doc', function(err, collection) {
-      collection.insert(sample);
+      collection.insert(sample, {safe:true}, function(err,result) {
+        log("insert doc to returns " + err, result);
+      });
     });
   });
 
